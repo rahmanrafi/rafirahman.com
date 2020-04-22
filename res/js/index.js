1064b21c1838a09e0cb74ef7d58287370ac2ef6f
Rafi Rahman <rafira@me.com>
2020-04-22 02:04:03 -0400

    Code cleanup and .js externalization.

---
+++ res/js/index.js
@@ -0,0 +1,134 @@
+// Typewriter effect
+const big_typer = new TypeIt('#typer-big', {
+    strings: [
+        'University of Toronto grad.',
+        'coder.',
+        'tinkerer.',
+        'writer.',
+        'film geek.',
+        'dog lover.',
+        'big thinker.',
+        'creator.'
+    ],
+    speed: 50,
+    breakLines: false,
+    waitUntilVisible: true,
+    loop: true,
+    nextStringDelay: 1500,
+    loopDelay: [
+        100, 2500
+    ],
+    startDelete: true,
+    startDelay: 500
+});
+const small_typer = new TypeIt('#typer-small', {
+    strings: [
+        'a creator.',
+        'a UofT grad.',
+        'a coder.',
+        'a tinkerer.',
+        'a writer.',
+        'a film geek.',
+        'a dog lover.',
+        'a big thinker.',
+        "<strong><a href=\"mailto:hello@rafirahman.com\" class=\"text-link\">Rafi Rahman.</a>"
+    ],
+    speed: 20,
+    breakLines: false,
+    waitUntilVisible: true,
+    loop: true,
+    nextStringDelay: 2500,
+    loopDelay: [
+        100, 2500
+    ],
+    startDelete: true,
+    startDelay: 1500
+});
+mobile_width = 1000;
+current_typer = (window.innerWidth <= mobile_width ? small_typer : big_typer);
+current_typer.go();
+window.onresize = function () {
+    var refresh_flag = false;
+    if ((window.innerWidth <= mobile_width) && (current_typer != small_typer)) {
+        refresh_flag = true;
+        new_typer = small_typer;
+    } else if ((window.innerWidth > mobile_width) && (current_typer != big_typer)) {
+        refresh_flag = true;
+        new_typer = big_typer;
+    }
+    if (refresh_flag === true) {
+        console.log('Toggling alternate typer element')
+        current_typer.destroy();
+        current_typer = new_typer;
+        current_typer.go();
+    }
+}
+// Vanta.js effects
+if (window.innerWidth <= 1000) {
+    VANTA.NET({
+        el: "body",
+        color: 0xaf0a75,
+        backgroundColor: 0x191c20,
+        points: 10,
+        maxDistance: 12,
+        spacing: 20
+    })
+} else {
+    VANTA.NET({
+        el: "body",
+        color: 0xaf0a75,
+        backgroundColor: 0x191c20,
+        points: 10,
+        maxDistance: 12,
+        spacing: 16
+    })
+}
+
+// Transition effect
+$(document).ready(function () {
+    if ($('.bottom-layer').hasClass('active')) {
+        var layers = document.querySelectorAll(".bottom-layer");
+        for (const layer of layers) {
+            layer.classList.toggle();
+        }
+    }
+})
+const resume_button = document.getElementById("resume-button")
+resume_button.onclick = function (e) {
+    e.preventDefault();
+    var href = $(this).attr('href');
+    var layers = document.querySelectorAll(".bottom-layer");
+    for (const layer of layers) {
+        layer.classList.toggle("active");
+    }
+    $('.bottom-layer--3').on('transitionend webkitTransitionEnd', function () {
+        window.location = href;
+    })
+}
+
+// Browser update notification
+var $buoop = {
+    required: {
+        e: -4,
+        f: -3,
+        o: -3,
+        s: -1,
+        c: -3
+    },
+    insecure: true,
+    style: "top",
+    api: 2020.02,
+    test: false,
+    text: "<b>Your browser ({brow_name}) is out of date.</b>  <a{up_but}>Update</a> <a{ignore_but}>Ignore</a>"
+};
+
+function $buo_f() {
+    var e = document.createElement("script");
+    e.src = "//browser-update.org/update.min.js";
+    document.body.appendChild(e);
+};
+try {
+    document.addEventListener("DOMContentLoaded", $buo_f, false)
+} catch (e) {
+    window.attachEvent("onload", $buo_f)
+}
---
+++ res/js/resume.js
@@ -0,0 +1,67 @@
+// Mobile download button animation
+var prevYOffset = window.pageYOffset;
+window.onscroll = function () {
+  var button = document.getElementById("dl-small");
+  if (window.pageYOffset == 0 || prevYOffset - 20 > window.pageYOffset) {
+    button.style.visibility = "visible";
+    button.style.opacity = 1;
+    button.style.bottom = "2.5%"
+  } else if (prevYOffset < window.pageYOffset) {
+    button.style.visibility = "hidden";
+    button.style.opacity = 0;
+    button.style.bottom = "5%"
+  }
+  prevYOffset = window.pageYOffset;
+};
+
+// Transition animation
+$(document).ready(function () {
+  $(".animsition-overlay").animsition({
+    inClass: 'overlay-slide-in-top',
+    outClass: 'overlay-slide-out-bottom',
+    inDuration: 250,
+    outDuration: 400,
+    linkElement: '.animsition-link',
+    loading: false,
+    loadingParentElement: 'body',
+    loadingClass: 'animsition-loading',
+    loadingInner: '',
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: ['animation-duration', '-webkit-animation-duration'],
+    overlay: true,
+    overlayClass: 'animsition-overlay-slide',
+    overlayParentElement: 'body',
+    transition: function (url) {
+      window.location.href = url;
+    }
+  });
+});
+
+// Browser update notifier
+var $buoop = {
+  required: {
+    e: -4,
+    f: -3,
+    o: -3,
+    s: -1,
+    c: -3
+  },
+  insecure: true,
+  style: "top",
+  api: 2020.02,
+  test: false,
+  text: "<b>Your browser ({brow_name}) is outdated and may break this page.</b> <a{up_but}>Update</a> <a href=\"resume.pdf\" id=\"emergency-button\">View PDF</a> <a{ignore_but}>Ignore</a>"
+};
+
+function $buo_f() {
+  var e = document.createElement("script");
+  e.src = "//browser-update.org/update.min.js";
+  document.body.appendChild(e);
+};
+try {
+  document.addEventListener("DOMContentLoaded", $buo_f, false)
+} catch (e) {
+  window.attachEvent("onload", $buo_f)
+}
